da0e45d15ae1056f5dc739711199f340863c36d7
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2022-02-21 13:01:02 -0500

    Before webpack code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-    mode: "development",
+	mode: "development",
 	devtool: "inline-source-map", // omit in production
 	entry: {
-		app: "./src/index.js",
+		index: "./src/index.js",
+		another: "./src/another-module.js",
 	},
 	devServer: {
 		static: "./dist",
